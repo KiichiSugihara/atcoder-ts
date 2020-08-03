500d49f43844f22f91367eb234a579bb153627a5
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-08-04 00:19:25 +0900

    feat(abc174): setup c

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {};
 
-config.PROBLEM_URL = "https://atcoder.jp/contests/abc174/tasks/abc174_b";
-config.PROBLEM_NAME = "abc174_b";
+config.PROBLEM_URL = "https://atcoder.jp/contests/abc174/tasks/abc174_c";
+config.PROBLEM_NAME = "abc174_c";
 
 module.exports = config;
