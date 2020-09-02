c5df14296380af41cb81f9cbfbd1bd85bd6bbcd4
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-09-02 22:08:28 +0900

    chore (abc145): setup a

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {};
 
-config.PROBLEM_URL = "https://atcoder.jp/contests/abc174/tasks/abc174_c";
-config.PROBLEM_NAME = "abc174_c";
+config.PROBLEM_URL = "https://atcoder.jp/contests/abc145/tasks/abc145_a";
+config.PROBLEM_NAME = "abc145_a";
 
 module.exports = config;
