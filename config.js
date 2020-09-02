df26c567c5586ed0de85dc46617ebd2c1216b5ce
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-09-02 22:23:15 +0900

    add(abc145): setup b

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {};
 
-config.PROBLEM_URL = "https://atcoder.jp/contests/abc145/tasks/abc145_a";
-config.PROBLEM_NAME = "abc145_a";
+config.PROBLEM_URL = "https://atcoder.jp/contests/abc145/tasks/abc145_b";
+config.PROBLEM_NAME = "abc145_b";
 
 module.exports = config;
---
+++ src/abc145_b/main.ts
@@ -0,0 +1,49 @@
+{
+  type Print = void | string | number | string[] | number[];
+  const outs: Print[] = [];
+  const _io = (i = 0) => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const str = require("fs").readFileSync("/dev/stdin", "utf8");
+    const lines = str.trim().split("\n");
+    const s = () => lines[i++] || "";
+    const n = () => Number(s());
+
+    const mn = (v: string[]) => v.map(Number);
+    const sp = (v: string) => v.split(" ");
+
+    const ss = () => sp(s());
+    const nn = () => mn(ss());
+    const nls = () => mn(lines.slice(i));
+    const nnls = () => lines.slice(i).map((v: string) => mn(sp(v)));
+
+    return { lines, s, n, ss, nn, nls, nnls };
+  };
+  const main = (): Print => {
+    const r = _io();
+    const [s] = r.ss();
+    // 以下入れ替え
+    return checkOnAirConditioner(Number(s));
+  };
+  /**
+   * check the degree  to turn on AirConditioner
+   *
+   * @param degree - The degree
+   * @returns "Yes" or "No"
+   *
+   */
+  const checkOnAirConditioner = (degree: number): string => {
+    if (degree >= 30) {
+      return "Yes";
+    } else {
+      return "No";
+    }
+  };
+
+  outs.push(main());
+
+  console.log(
+    outs
+      .map((line) => (typeof line === "object" ? line.join(" ") : String(line)))
+      .join("\n")
+  );
+}
