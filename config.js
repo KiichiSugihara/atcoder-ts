4d49328d02f0cdfd4ee181bd269ea959a1a3d7d8
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-01-26 13:54:22 +0900

    setup 081_a

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {};
 
-config.PROBLEM_URL = "https://atcoder.jp/contests/abc086/tasks/abc086_a";
-config.PROBLEM_NAME = "abc086_a";
+config.PROBLEM_URL = "https://atcoder.jp/contests/abc081/tasks/abc081_a";
+config.PROBLEM_NAME = "abc081_a";
 
 module.exports = config;
