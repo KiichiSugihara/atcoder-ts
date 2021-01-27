b08d6690082101788806398ec2b2417c0d787709
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-01-27 10:32:30 +0900

    setup abc088b

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {};
 
-config.PROBLEM_URL = "https://atcoder.jp/contests/abc083/tasks/abc083_b";
-config.PROBLEM_NAME = "abc083_b";
+config.PROBLEM_URL = "https://atcoder.jp/contests/abc088/tasks/abc088_b";
+config.PROBLEM_NAME = "abc088_b";
 
 module.exports = config;
