512ab1e4c2a76514dc89a30ab26354f7405b24fa
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-01-27 16:15:16 +0900

    setup abc085c input

--- src/abc085_c/main.ts
+++ src/abc085_c/main.ts
@@ -9,35 +9,19 @@
   };
   const main = (): Print => {
     const args = _io().lines;
-    const [mochies] = [
-      args
-        // ０個目の餅数排除
-        .filter((_: string, index: number) => index > 0)
-        // 要素をnumberに変換
-        .map((o: string) => parseInt(o, 10)),
+    const [paperNumber, sumOtoshidama] = [
+      args[0].split(" ")[0],
+      args[0].split(" ")[1],
     ];
-    return kagamiMochi(mochies);
+    return checkOtoshidama(paperNumber, sumOtoshidama);
   };
 
-  const kagamiMochi = (mochies: number[]): number => {
-    // 降順で並べる
-    mochies.sort((a, b) => b - a);
-    let count = 0;
-    let currentWidth: number | undefined;
-    mochies.forEach((mochiWidth) => {
-      // 最初
-      if (currentWidth === undefined) {
-        currentWidth = mochiWidth;
-        count += 1;
-        return;
-      }
-      if (currentWidth === mochiWidth) {
-        return;
-      }
-      currentWidth = mochiWidth;
-      count += 1;
-    });
-    return count;
+  const checkOtoshidama = (
+    paperNumber: number,
+    sumOtoshidama: number
+  ): string => {
+    console.log(paperNumber, sumOtoshidama);
+    return "4 0 5";
   };
 
   outs.push(main());
