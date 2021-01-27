08b9b731e90c69c8d0a007909146900fbd89a450
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-01-27 13:01:37 +0900

    chore:improve template for Peformance

--- templates/main.ts
+++ templates/main.ts
@@ -1,43 +1,43 @@
 {
   type Print = void | string | number | string[] | number[];
   const outs: Print[] = [];
-  const _io = (i = 0) => {
+  const _io = () => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const str = require("fs").readFileSync("/dev/stdin", "utf8");
     const lines = str.trim().split("\n");
-    const s = () => lines[i++] || "";
-    const n = () => Number(s());
-
-    const mn = (v: string[]) => v.map(Number);
-    const sp = (v: string) => v.split(" ");
-
-    const ss = () => sp(s());
-    const nn = () => mn(ss());
-    const nls = () => mn(lines.slice(i));
-    const nnls = () => lines.slice(i).map((v: string) => mn(sp(v)));
-
-    return { lines, s, n, ss, nn, nls, nnls };
+    return { lines };
   };
   const main = (): Print => {
-    const r = _io();
-    const s = r.nnls();
-    const [numberA, numberB, numberC, text] = [
-      s[0][0],
-      s[1][0],
-      s[1][1],
-      r.lines[2],
+    const args = _io().lines;
+    const [mochies] = [
+      args
+        // ０個目の餅数排除
+        .filter((_: string, index: number) => index > 0)
+        // 要素をnumberに変換
+        .map((o: string) => parseInt(o, 10)),
     ];
-    // 以下入れ替え
-    return practice01(numberA, numberB, numberC, text);
+    return kagamiMochi(mochies);
   };
 
-  const practice01 = (
-    numberA: number,
-    numberB: number,
-    numberC: number,
-    text: string
-  ): string => {
-    return String(numberA + numberB + numberC) + " " + text;
+  const kagamiMochi = (mochies: number[]): number => {
+    // 降順で並べる
+    mochies.sort((a, b) => b - a);
+    let count = 0;
+    let currentWidth: number | undefined;
+    mochies.forEach((mochiWidth) => {
+      // 最初
+      if (currentWidth === undefined) {
+        currentWidth = mochiWidth;
+        count += 1;
+        return;
+      }
+      if (currentWidth === mochiWidth) {
+        return;
+      }
+      currentWidth = mochiWidth;
+      count += 1;
+    });
+    return count;
   };
 
   outs.push(main());
