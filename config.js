56f9b130219958be25ef2a3ec3b4b01468baa60b
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-01-29 14:21:16 +0900

    feat: 🎸 setup arc089a

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {};
 
-config.PROBLEM_URL = "https://atcoder.jp/contests/abc049/tasks/arc065_a";
-config.PROBLEM_NAME = "arc065_a";
+config.PROBLEM_URL = "https://atcoder.jp/contests/abc086/tasks/arc089_a";
+config.PROBLEM_NAME = "arc089_a";
 
 module.exports = config;
