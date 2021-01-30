22e131a3acc050b301180bd48f5a59cadee98795
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-01-30 21:21:07 +0900

    setup abc190b

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {};
 
-config.PROBLEM_URL = "https://atcoder.jp/contests/abc190/tasks/abc190_a";
-config.PROBLEM_NAME = "abc190_a";
+config.PROBLEM_URL = "https://atcoder.jp/contests/abc190/tasks/abc190_b";
+config.PROBLEM_NAME = "abc190_b";
 
 module.exports = config;
