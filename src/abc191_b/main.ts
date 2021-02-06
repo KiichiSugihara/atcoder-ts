32f537e319284872d1dd573d1c76e1c4bf1bc46c
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-02-06 21:27:53 +0900

    feat: 🎸 setup abc191b

---
+++ src/abc191_b/main.ts
@@ -0,0 +1,62 @@
+import * as fs from "fs";
+{
+  type Print = void | string | number | string[] | number[];
+  const outs: Print[] = [];
+  const str = fs.readFileSync("/dev/stdin", "utf8");
+
+  // Inputに対する共通処理
+  class Input {
+    lines: string[];
+    index = 0;
+    constructor(str: string) {
+      this.lines = str.split("\n");
+    }
+    // 使うとindex足されて、index+1の行の文字列取得
+    s = () => this.lines[this.index++] || "";
+    // 使うとindex足されて、index+1の行のnumber取得
+    n = () => Number(this.s());
+    // string[]→number[]
+    mn = (v: string[]) => v.map(Number);
+    // "a b c ..."→['a','b','c',...]
+    sp = (v: string) => v.split(" ");
+
+    ss = () => this.sp(this.s());
+    nn = () => this.mn(this.ss());
+    // indexの行の文字列取得
+    nls = () => this.mn(this.lines.slice(this.index));
+    // 前半別の方法で取得も良い
+    // number[][]で扱いたい時
+    nnls = () =>
+      this.lines.slice(this.index).map((v: string) => this.mn(this.sp(v)));
+  }
+
+  const input = new Input(str);
+
+  const main = (): Print => {
+    // 問題のための入力処理
+
+    const [n, x, a] = [
+      input.lines[0].split(" ")[0],
+      input.lines[0].split(" ")[1],
+      input.lines[1],
+    ];
+
+    // 問題のための関数
+    return removeIt(Number(n), x, a);
+  };
+
+  // 問題のための関数
+  const removeIt = (n: number, x: string, a: string): string => {
+    console.log(n, x);
+    console.log(a);
+    return a;
+  };
+
+  outs.push(main());
+
+  console.log(
+    outs
+      .map((line) => (typeof line === "object" ? line.join(" ") : String(line)))
+      .join("\n")
+  );
+}
