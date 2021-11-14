5028313706c79999ad0550bfeb08b77f8d3e76a2
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2021-11-15 07:54:52 +0900

    feat: 🎸 setup 226-b

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {};
 
-config.PROBLEM_URL = "https://atcoder.jp/contests/abc226/tasks/abc226_a";
-config.PROBLEM_NAME = "abc226_a";
+config.PROBLEM_URL = "https://atcoder.jp/contests/abc226/tasks/abc226_b";
+config.PROBLEM_NAME = "abc226_b";
 
 module.exports = config;
