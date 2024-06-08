67e096d574a5532e856ea071229beb93753771f8
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2024-06-08 10:30:35 +0900

    feat: 🎸 setup 356-b and renew template

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let config = {};
 
-config.PROBLEM_URL = "https://atcoder.jp/contests/abc356/tasks/abc356_a";
-config.PROBLEM_NAME = "abc356_a";
+config.PROBLEM_URL = "https://atcoder.jp/contests/abc356/tasks/abc356_b";
+config.PROBLEM_NAME = "abc356_b";
 
 module.exports = config;
